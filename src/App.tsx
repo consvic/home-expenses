bbd6b0e1b494dbef37482693337a661aff1ef435
consvic <cocoy_shuffle@me.com>
2023-06-05 23:51:03 -0600

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <div className="App">App</div>
+    </ChakraProvider>
+  );
+}
+
+export default App;
