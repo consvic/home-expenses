bc9d365574ab58d5be003f4e6fc2d5dec7a43a96
consvic <cocoy_shuffle@me.com>
2023-06-10 17:23:18 -0600

    calculate total per month

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -67,7 +67,77 @@ export function Expenses() {
     {}
   )
 
-  console.log({ frequentExpenses })
+  function getTotalAmountPerMonth() {
+    // calculate the total amount per month taking account of the installments per each item
+    const allExpenses: Array<{
+      concept: string
+      amount: number
+      paidBy: string
+      month: number
+      installments?: number
+    }> = [...expenses, ...frequentExpenses]
+    const addPaymentWithInstallments = allExpenses.reduce(
+      (
+        acc: Array<{
+          concept: string
+          amount: number
+          paidBy: string
+          month: number
+          installments?: number
+        }>,
+        item
+      ) => {
+        const installments = item?.installments
+        if (installments && installments > 1) {
+          const newExpenses = Array.from(Array(installments).keys()).map(
+            (month) => {
+              return {
+                ...item,
+                amount: item.amount / installments,
+                installments: 0,
+                month: month + item.month,
+              }
+            }
+          )
+          return [...acc, ...newExpenses]
+        } else {
+          return [...acc, item]
+        }
+      },
+      []
+    )
+    const grouppedByMonth = addPaymentWithInstallments.reduce(
+      (
+        acc: {
+          [key: string]: Array<{
+            concept: string
+            amount: number
+            paidBy: string
+            month: number
+            installments?: number
+          }>
+        },
+        item
+      ) => {
+        const { month } = item
+        if (!acc[month]) {
+          acc[month] = []
+        }
+        acc[month].push(item)
+        return acc
+      },
+      {}
+    )
+    let sumsPerMonth: Record<string, number> = {}
+    for (const month in grouppedByMonth) {
+      let sum = 0
+      grouppedByMonth[month].forEach((expense) => {
+        sum += expense.amount
+      })
+      sumsPerMonth[month] = sum
+    }
+    return sumsPerMonth
+  }
 
   return (
     <Box paddingTop={6}>
